46084bec2b77e49fc42d37d784caa19d0d7bc346
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-03-26 20:42:54 +0200

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,11 +12,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes_left, nodes_right;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-
-	if ((tree->left != NULL || tree->right != NULL))
-		return (1);
 
 	nodes_left = binary_tree_nodes(tree->left);
 	nodes_right = binary_tree_nodes(tree->right);
