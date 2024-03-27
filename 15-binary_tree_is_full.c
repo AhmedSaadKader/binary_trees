23d2f6bf822b9679b9d9f4d70175bbba10e69ebb
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-03-27 14:29:04 +0200

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,9 +18,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (tree->left && tree->right)
+	{
 		left_full = binary_tree_is_full(tree->left);
 		right_full = binary_tree_is_full(tree->right);
 		return (left_full && right_full);
+	}
 
 	return (0);
 }
